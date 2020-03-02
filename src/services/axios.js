e40846894c01216394edc7cd6e58e9b99ebf5bea
David <yogasakajr@gmail.com>
2020-03-02 17:12:59 +0700

    add axios services

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export const Instance = axios.create({
-  baseURL: 'http://localhost:8000/api/',
-  timeout: 1000,
-  headers: {'X-Custom-Header': 'foobar'}
+  baseURL: 'http://192.168.1.11:8000/api/',
+  timeout: 5000
 });
