7e290ac7155765f2e19d0d99f919fec83580d625
David Arayoga <yogasakajr@gmail.com>
2020-03-06 11:17:05 +0700

    fixing logoSlide

--- src/component/slider/LogoSlider.js
+++ src/component/slider/LogoSlider.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Slider from 'react-slick';
 
-import img1 from './assets/tmp_partner_logo/sakura.png'
-import img2 from './assets/tmp_partner_logo/leopalace.png'
-import img3 from './assets/tmp_partner_logo/cgs.png'
 import { BaseUrl } from '../../services/axios';
 
 const LogoSlider = (props) => {
@@ -20,24 +17,23 @@ const LogoSlider = (props) => {
   };
 
   const { store, errors } = props
-  // if (Object.keys(errors).length) {
-  //   return (
-  //     <div>
-  //       <h4>Errors in Partnership Logo</h4>
-  //       <p>{errors.code}</p>
-  //       <p>{errors.status}</p>
-  //     </div>
-  //   )
-  // }
+  if (Object.keys(errors).length) {
+    return (
+      <div>
+        <h4>Errors in Partnership Logo</h4>
+        <p>{errors.code}</p>
+        <p>{errors.status}</p>
+      </div>
+    )
+  }
   return(
     <div style={{margin:"150px 0px",height:"260px"}}>
       <Slider {...settings}>
         {console.log(store)}
-        {logos.map((item, i) => (
+        {store.map((item, i) => (
           <div key={i}>
             <div>
-              {/* <img src={BaseUrl + '/storage/' + item.gambar} alt={item.nama + '-' + item.unit.unit_name} /> */}
-              <a href={item.link}><img style={{margin:"0px auto", maxHeight:"200px"}} src={item.image.img1} alt={item.name} /></a>
+              <a href={item.link}><img style={{margin:"0px auto", maxHeight:"200px"}} src={BaseUrl + '/storage/' + item.image} alt={item.name.replace(" ", "-")} /></a>
             </div>
           </div>
         ))}
@@ -46,30 +42,3 @@ const LogoSlider = (props) => {
   )
 }
 export default LogoSlider
-
-const logos = [
-  {
-    "id":1,
-    "name":"Bank BTN",
-    "link":"#link",
-    "image":{img1},
-    "created_at":"2020-03-02 09:43:59",
-    "updated_at":"2020-03-02 09:43:59"
-  },
-  {
-    "id":2,"name":"Bank Mandiri","link":"#mandiri",
-    "image":{img1},"created_at":"2020-03-02 09:44:17","updated_at":"2020-03-02 09:44:17"
-  },
-  {
-    "id":3,"name":"Central Graga Sejahtera","link":"#linkto",
-    "image":{img1},"created_at":"2020-03-02 09:44:43","updated_at":"2020-03-02 09:44:43"
-  },
-  {
-    "id":4,"name":"Leopalace 21","link":"#linkto",
-    "image":{img1},"created_at":"2020-03-02 09:45:02","updated_at":"2020-03-02 09:45:02"
-  },
-  {
-    "id":5,"name":"Sakura Project Management","link":"#linkto",
-    "image":{img1},"created_at":"2020-03-02 09:45:23","updated_at":"2020-03-02 09:45:23"
-  }
-]
