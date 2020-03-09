ba01f8dfe9df9d7afeb42220c775c797cf716216
David Arayoga <yogasakajr@gmail.com>
2020-03-09 23:16:35 +0700

    fixing mobile gallery

--- src/component/slider/mobile/Gallery.js
+++ src/component/slider/mobile/Gallery.js
@@ -3,105 +3,13 @@ import Slider from 'react-slick'
 import styled from 'styled-components'
 import { BaseUrl } from '../../../services/axios'
 
+import '../assets/css/mobileStyles.css'
+
 // dummy Data
 import Gal1 from './galleryDummy/1.svg'
 import Gal2 from './galleryDummy/2.svg'
 import Gal3 from './galleryDummy/3.svg'
 
-const NextArrow = (props) => {
-  const { className, style, onClick } = props;
-  const Arrows = styled.div({
-    display: "inline-block",
-    top:"40%",
-    transition:".3s all",
-    width:"70px",
-    height:"70px",
-    background:"#CC9980",
-    color: "#fff",
-    textDecoration: "none",
-    padding: "25px",
-    borderRadius: "50%",
-    boxShadow: "0px 20px 60px rgba(138, 149, 158, 0.2)",
-    right:"10%",
-    '&:hover': {
-      background:"#ffffff",
-      color: "#000"
-    },
-    '&:before': {
-      padding: "8px",
-      borderRight: "4px solid",
-      borderBottom: "4px solid",
-      borderRadius: "4px",
-      content: `'' !important`,
-      display: "block",
-      top: "35%",
-      left: "30%",
-      position: "absolute",
-      '-moz-transform': "rotate(310deg)",
-      '-o-transform': "rotate(310deg)",
-      '-webkit-transform': "rotate(310deg)",
-      transform: "rotate(310deg)",
-      color:"inherit"
-    }
-  });
-  return (
-    <Arrows
-      className={className}
-      style={{ 
-        ...style,
-      }}
-      onClick={onClick}
-    />
-  );
-}
-
-const PrevArrow = (props) => {
-  const { className, style, onClick } = props;
-  const Arrows = styled.div({
-    display: "inline-block",
-    top:"40%",
-    transition:".3s all",
-    width:"70px",
-    height:"70px",
-    background:"#CC9980",
-    color: "#ffffff",
-    textDecoration: "none",
-    padding: "30px",
-    zIndex:"1",
-    borderRadius:"50%",
-    left:"10%",
-    boxShadow: "0px 20px 60px rgba(138, 149, 158, 0.2)",
-    '&:hover': {
-      background:"#ffffff",
-      color: "#000"
-    },
-    '&:before': {
-      padding: "8px",
-      borderRight: "4px solid",
-      borderBottom: "4px solid",
-      borderRadius: "4px",
-      content: `'' !important`,
-      display: "block",
-      top: "35%",
-      left: "40%",
-      position: "absolute",
-      '-moz-transform': "rotate(135deg)",
-      '-o-transform': "rotate(135deg)",
-      '-webkit-transform': "rotate(135deg)",
-      transform: "rotate(135deg)",
-      color:"inherit"
-    }
-  });
-  return (
-    <Arrows
-      className={className}
-      style={{ 
-        ...style,
-      }}
-      onClick={onClick}
-    />
-  );
-}
 
 export class MobileGallery extends Component {
   constructor(props) {
@@ -113,19 +21,16 @@ export class MobileGallery extends Component {
     }
 
     this.settings = {
-        dots: true,
-        className: "center",
-        centerMode: true,
-        centerPadding: "100px",
-        slidesToShow: 1,
-        speed: 1500,
-        nextArrow: false,
-        prevArrow: false,
-        arrows:false,
-        lazyLoad: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        pauseOnHover: true,
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 1,
+      speed: 1500,
+      autoplay: true,
+      speed: 4000,
+      autoplaySpeed: 5000,
+      arrows: false,
     };
   }
 
@@ -165,13 +70,9 @@ export class MobileGallery extends Component {
                     {this.state.store.length && this.state.store.map((item, i) => (
                         <div>
                             <img 
-                                src={BaseUrl + '/storage/' + item.gambar} 
-                                // src={item.gambar}
-                                style={{
-                                    width: "100%",
-                                    height: "auto",
-                                }} 
+                                src={BaseUrl + '/storage/' + item.gambar}
                                 alt={item.nama + '-' + item.unit.unit_name} 
+                                style={{width:228}}
                             />
                         </div>
                     ))}
