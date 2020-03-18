c0b6cceb175e398780853d8fe93be66e44209e87
Davidasrobot <yogasakajr@gmail.com>
2020-03-18 07:04:02 +0700

    staging for audit

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BaseUrl = 'http://192.168.1.10:8000'
+export const BaseUrl = 'https://clevara.id'
 
 export const Instance = axios.create({
   baseURL: `${BaseUrl}/api/`,
