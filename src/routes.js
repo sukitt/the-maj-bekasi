7d9a2c429e099f76723dd0f8aeb9834d1c2a4329
sukitt <r.arifin123@gmail.com>
2020-03-20 16:25:23 +0700

    remove testing view

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,6 @@ import TentangKami from './view/TentangKami'
 import Partnership from './view/Partnership'
 import Expertice from './view/Expertice'
 import Blog from './view/Blog'
-import TestingForm from './view/TestingForm'
 
 const BaseRoute = () => {
     return (
@@ -16,7 +15,6 @@ const BaseRoute = () => {
             <Route exact path="/partnership" component={Partnership} />
             <Route exact path="/expertice" component={Expertice} />
             <Route exact path="/blog/:id" children={<Blog />} />
-            <Route exact path="/testing-form" component={TestingForm} />
         </div>
     )
 }
