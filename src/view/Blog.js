d1604b4405bb41797a0a5d84ef4315d837a0df2b
sukitt <r.arifin123@gmail.com>
2020-03-20 19:44:29 +0700

    modified blog mobile

--- src/view/Blog.js
+++ src/view/Blog.js
@@ -28,7 +28,6 @@ const Blog = props => {
         const { heading, image, img_desc, author, created_at, text  } = Detail
         return (
             <>
-            {/* <OnDesktop> */}
                 <section>
                     <div className="container">
                         <div style={{width: "auto", margin: "0px auto", padding: "20px 0"}}>
@@ -62,7 +61,7 @@ const Blog = props => {
                                 </Col>
 
                                 <OnDesktop>
-                                    <Col lg={4} >
+                                    <Col lg={4} style={{padding: "0"}}>
                                         <div>
                                         {List && List.map((data, i) => {
                                             let head = data.heading && data.heading.toLowerCase().replace(/\s/g, "-").replace(/[%@#,*>!?"'.]/g, "")
@@ -86,33 +85,6 @@ const Blog = props => {
                         </div>
                     </div>
                 </section>
-            {/* </OnDesktop> */}
-            
-            {/* <OnMobileAndTablet>
-                <section>
-                    <div className="container">
-                        <div style={{padding: "20px 0"}}>
-                            <MobileDetails {...Data} />
-                            {location.state && location.state.store.map((data, i) => {
-                                let head = data.heading && data.heading.toLowerCase().replace(/\s/g, "-")
-                                let updated = data.updated_at.replace(/[\s:]/g, "-")
-                                return (
-                                    <BlogItem 
-                                        key={i} 
-                                        src={`${BaseUrl}/storage/${data.image.replace(/\\/g, "/")}`} 
-                                        head={data.heading} 
-                                        posted_at={data.created_at}
-                                        to={{
-                                            pathname: `/blog/${head}-${updated}`,
-                                            state: { store: location.state.store}
-                                        }}
-                                    />
-                                )
-                            })}
-                        </div>
-                    </div>
-                </section>
-            </OnMobileAndTablet> */}
             </>
         )
     
@@ -125,74 +97,6 @@ const renderHTML = (args) => {
         dangerouslySetInnerHTML: {__html: args}
     })
 }
-
-const Details = ({id, heading, image, img_desc, author, created_at, text}) => {
-    
-    return (
-        <>
-            <Col lg={12}>
-                <div>
-                    <H2 margin="0px 0px 41px 0px"> {heading} </H2>
-                    <img src={`${BaseUrl}/storage/${image.replace(/\\/g, "/")}`} width="1103px" height="465px" alt="blog-img" />
-                    <DescImage>
-                        {img_desc}
-                    </DescImage>
-                </div>
-            </Col>
-
-            <Col lg={8} style={{marginRight: "19.5"}}>
-                <div style={{padding: "86px 0 134px 0"}}>
-                    <div style={{display: "flex", marginBottom: "21px"}}>
-                        <Col lg={6}>
-                            <Author author={author}  />
-                        </Col>
-                        <Col lg={6}>
-                            <Posted_At posted_at={created_at} />
-                        </Col>
-                    </div>
-                    <div>
-                        {text && renderHTML(text)}
-                    </div>
-                </div>
-            </Col>
-        </>
-    )
-}
-
-
-const MobileDetails = ({id, heading, author, created_at, image, img_desc, text}) => {
-    const renderHTML = (args) => {
-        return createElement("div", {
-            dangerouslySetInnerHTML: {__html: args}
-        })
-    }
-
-    return (
-        <>
-            <div style={{margin: "50px 0 13px 0"}}>
-                <H2 margin="0px auto 33px auto"> {heading} </H2>
-                <div>
-                    <img src={`${BaseUrl}/storage/${image.replace(/\\/g, "/")}`} width="100%" height="auto" alt="blog-img" />
-                </div>
-            </div>
-
-            <div>
-                <div style={{marginTop: "43px", marginBottom: "30px", flexDirection: "column"}}>
-                    <Col xs={12} sm={6} lg={6} style={{padding: 0}}>
-                        <Author author={author}  />
-                    </Col>
-                    <Col xs={12} sm={6} lg={6} style={{padding: 0}}>
-                        <Posted_At posted_at={created_at} />
-                    </Col>
-                </div>
-                <div style={{marginTop: "30px"}}>
-                    {text && renderHTML(text)}
-                </div>
-            </div>
-        </>
-    )
-}
-
 
 const Author = (props) => (
     <>
@@ -232,11 +136,15 @@ const Posted_At = props => (
 
 const BlogItem = props => (
     <ContainerItem>
-        <Col lg={4}>
+        <Col lg={4} style={{padding: "0"}}>
             <img src={props.src} width="95px" height="95px" alt="list-blog" />
         </Col>
-        <Col lg={8}>
-            <H2> <Link {...props}> {props.head} </Link> </H2>
+        <Col lg={8} style={{padding: "0"}}>
+            <Link {...props} style={{textDecoration: "none"}}> 
+                <H3> 
+                    {props.head} 
+                </H3>
+            </Link> 
             <Created> {props.posted_at} </Created>
         </Col>
     </ContainerItem>
@@ -259,19 +167,8 @@ const ContainerItem = styled.div(
     props => ({
         display: "flex",
         margin: "0 0 35px 0",
-        padding: "0 0 35px 0",
+        padding: "0 0 27px 0",
         borderBottom: "1px solid #C8C8C8",
-    })
-)
-
-const Head = styled.h2(
-    props => ({
-        margin: props.margin,
-        padding: props.padding,
-        fontStyle: "normal",
-        fontWeight: "bold",
-        lineHeight: "28px",
-        color: "#00000",
     })
 )
 
@@ -285,15 +182,19 @@ const H2 = styled.h2(
     })
 )
 
-const P = styled.p(
-    props => ({
-        fontStyle: "normal", 
-        fontWeight: "normal", 
-        fontSize: "16px", 
-        lineHeight: "21px", 
-        color: "#000000",
+const H3 = styled.h3(
+    props => ({  
+        // fontSize: "15px",
+        height: "61px",
+        whiteSpace: "wrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
         margin: props.margin,
-        padding: props.padding
+        padding: props.padding,
+        transition: "color .2s",
+        ":hover": {
+            color: "#CC9980",
+        }
     })
 )
 
