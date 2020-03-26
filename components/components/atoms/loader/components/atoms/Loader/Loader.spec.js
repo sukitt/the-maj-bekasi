e2abb587f5d84c2072ea1612f09b20bf9a4f53db
sukitt <r.arifin123@gmail.com>
2020-03-26 21:27:54 +0700

    remove component loader

--- components/components/atoms/loader/components/atoms/Loader/Loader.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Loader from './Loader'
-
-describe('<Loader />', () => {
-  it('renders without crashing', () => {
-    expect(() => shallow(<Loader />)).not.toThrow()
-  })
-})
