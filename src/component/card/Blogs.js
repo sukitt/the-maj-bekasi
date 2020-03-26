b0c18d0b4724d945e8449b75d7a3574cdf310ce2
sukitt <r.arifin123@gmail.com>
2020-03-27 03:31:33 +0700

    Add readmore button

--- src/component/card/Blogs.js
+++ src/component/card/Blogs.js
@@ -28,7 +28,7 @@ class Blogs extends Component {
     render() {
         return (
             <Container id="blogs" margin="105px 0 0 0"  flexDirection="column">
-                <H1 margin="50px auto">Lates News</H1>
+                <H1 margin="50px auto">Update Terbaru</H1>
                 <Row style={{marginTop: "48px"}}>
                     {this.state.localStore.length && this.state.localStore.slice(0,3).map((data, i) => {
                         
@@ -62,18 +62,32 @@ export default Blogs
 const Blog = props => {
     return (
         <Container flexDirection="column">
-            <Link {...props} >
-                <Img src={props.src} width width="350px" height="350px" alt="350x350" />
-                <Body margin="17px 0 0 0">
-                    <P>
-                        {props.heading}
-                    </P>
-                </Body>
-            </Link>
-            <Footer>Posted On {props.created_at}</Footer>
+            <Img src={props.src} width width="350px" height="350px" alt="350x350" />
+            <Body margin="17px 0 20px 0">
+                <P>{props.heading}</P>
+                <Footer>Posted On {props.created_at}</Footer>
+            </Body>
+            <LinkRead {...props}> Read More </LinkRead>
         </Container>
     )
 }
+
+const LinkRead = styled(Link)({
+    alignSelf: "center",
+    fontFamily: "Verlag Bold",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: "11px",
+    textDecoration: "none",
+    textAlign: "center",
+    textTransform: "uppercase",
+    letterSpacing: "1px",
+    paddingBottom: "14px",
+    borderBottom: "1px solid",
+    ":hover": {
+        textDecoration: "none",
+    }
+})
 
 const Container = styled.div(
     props => ({
@@ -88,6 +102,8 @@ const Container = styled.div(
 
 const Body = styled.div(
     props => ({
+        display: "flex",
+        flexDirection: "column",
         width: "350px",
         margin: props.margin,
         padding: props.padding,
@@ -121,10 +137,6 @@ const P = styled.p(
         fontStyle: "normal",
         fontWeight: "bold",
         color: "#12284C !important",
-        transition: "color .2s",
-        ":hover": {
-            color: "#CC9980 !important",
-        }
     })
 )
 
